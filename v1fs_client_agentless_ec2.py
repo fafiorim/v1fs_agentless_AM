d7c597b50272eecfd9e3efaa966d3bc8f61fcdc2
Franz Fiorim <franzvitor@gmail.com>
2024-02-28 22:21:03 -0500

    Update v1fs_client_agentless_ec2.py

--- v1fs_client_agentless_ec2.py
+++ v1fs_client_agentless_ec2.py
@@ -12,20 +12,12 @@ import socket  # Import socket module
 # Import boto3 library to access AWS services
 import boto3
 
-def setup_logging(hostname, instance_metadata):
+def setup_logging():
     now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     log_filename = f"logs/log_{now}.txt"
     os.makedirs(os.path.dirname(log_filename), exist_ok=True)
     logging.basicConfig(filename=log_filename, level=logging.INFO,
                         format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    # Write host information in log format at the beginning of the log file
-    with open(log_filename, 'w') as log_file:
-        log_file.write("########## HOST INFO ##########\n")
-        log_file.write(f"Hostname: {hostname}\n")
-        if instance_metadata:
-            for key, value in instance_metadata.items():
-                log_file.write(f"{key}: {value}\n")
-        log_file.write("*******************************\n")
     return os.path.abspath(log_filename)
 
 def get_instance_metadata():
@@ -35,14 +27,14 @@ def get_instance_metadata():
         response = ec2_client.describe_instances()
         instance_data = response['Reservations'][0]['Instances'][0]
         instance_metadata = {
-            'Instance ID': instance_data.get('InstanceId', 'N/A'),
-            'Instance Type': instance_data.get('InstanceType', 'N/A'),
-            'AMI ID': instance_data.get('ImageId', 'N/A'),
-            'Public IP Address': instance_data.get('PublicIpAddress', 'N/A'),
-            'Private IP Address': instance_data.get('PrivateIpAddress', 'N/A'),
+            'InstanceId': instance_data.get('InstanceId'),
+            'InstanceType': instance_data.get('InstanceType'),
+            'ImageId': instance_data.get('ImageId'),
+            'PublicIpAddress': instance_data.get('PublicIpAddress'),
+            'PrivateIpAddress': instance_data.get('PrivateIpAddress'),
             'Region': instance_data['Placement']['AvailabilityZone'][:-1],
-            'VPC ID': instance_data.get('VpcId', 'N/A'),
-            'Tags': instance_data.get('Tags', 'N/A')
+            'VpcId': instance_data.get('VpcId'),
+            'Tags': instance_data.get('Tags')
         }
         return instance_metadata
     except Exception as e:
@@ -55,6 +47,46 @@ def get_fqdn():
     except Exception as e:
         logging.error(f"Error retrieving FQDN: {e}")
         return None
+
+def print_summary(scanned_files, excluded_files, malicious_files, clean_files, grpc_time, total_elapsed, log_file_path, instance_metadata=None, fqdn=None):
+    print(f"\n########## SUMMARY ##########")
+    print(f"\n ********** HOST INFO **********")
+    if fqdn:
+        print(f"Hostname (FQDN): {fqdn}")
+    if instance_metadata:
+        print("Instance Metadata:")
+        print(f"\tInstance ID: {instance_metadata.get('InstanceId', 'N/A')}")
+        print(f"\tInstance Type: {instance_metadata.get('InstanceType', 'N/A')}")
+        print(f"\tAMI ID: {instance_metadata.get('ImageId', 'N/A')}")
+        print(f"\tPublic IP: {instance_metadata.get('PublicIpAddress', 'N/A')}")
+        print(f"\tPrivate IP: {instance_metadata.get('PrivateIpAddress', 'N/A')}")
+        print(f"\tRegion: {instance_metadata.get('Region', 'N/A')}")
+        print(f"\tVPC ID: {instance_metadata.get('VpcId', 'N/A')}")
+        print(f"\tTags: {instance_metadata.get('Tags', 'N/A')}")
+    print(f"******************************")
+    print(f"\n********** SCAN **********")
+    print(f"Total files scanned: {len(scanned_files)}")
+    print(f"Files excluded: {len(excluded_files)}")
+    print(f"Malicious files: {len(malicious_files)}")
+    print(f"Clean files: {len(clean_files)}")
+    print(f"Total scan time: {grpc_time:0.2f} seconds")
+    print(f"Total execution time: {total_elapsed:0.2f} seconds")
+    print(f"Log file: {log_file_path}")
+    print(f"******************************")
+
+    if excluded_files:
+        print(f"\n********** EXCLUSIONS **********")
+        print("Excluded Files:")
+        for file_path in excluded_files:
+            print(f"\t{file_path}")
+        print(f"******************************")
+    if malicious_files:
+        print(f"\n********** DETECTIONS **********")
+        print("Malicious Files:")
+        for file_path in malicious_files:
+            print(f"\t{file_path}")
+        print(f"******************************")
+    print(f"\n#############################\n")
 
 def scan_directory(directory, exclude, addr, region, api_key, tls, ca_cert, tags):
     scanned_files = []
@@ -96,11 +128,7 @@ def scan_directory(directory, exclude, addr, region, api_key, tls, ca_cert, tags
     return scanned_files, excluded_files, malicious_files, clean_files, grpc_time
 
 if __name__ == "__main__":
-    fqdn = get_fqdn()
-    instance_metadata = get_instance_metadata()  # Retrieve EC2 instance metadata
-    hostname = fqdn if fqdn else socket.gethostname()
-
-    log_file_path = setup_logging(hostname, instance_metadata)
+    log_file_path = setup_logging()
 
     parser = argparse.ArgumentParser(description="Scan files in a directory with optional exclusion of file types.")
     parser.add_argument('--directory', '-d', required=True, help='Directory to scan')
@@ -114,6 +142,17 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
+    fqdn = get_fqdn()
+    if fqdn:
+        hostname_tag = f"hostname={fqdn}"
+        if args.tags:
+            args.tags.append(hostname_tag)
+        else:
+            args.tags = [hostname_tag]
+
+    instance_metadata = get_instance_metadata()  # Retrieve EC2 instance metadata
     total_start_time = time.perf_counter()
     scanned_files, excluded_files, malicious_files, clean_files, grpc_time = scan_directory(args.directory, args.exclude, args.addr, args.region, args.api_key, args.tls, args.ca_cert, args.tags)
     total_elapsed = time.perf_counter() - total_start_time
+
+    print_summary(scanned_files, excluded_files, malicious_files, clean_files, grpc_time, total_elapsed, log_file_path, instance_metadata, fqdn)
